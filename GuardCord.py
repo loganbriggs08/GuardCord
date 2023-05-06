a0ee7c0d3c4d68bfb44d31db6740c6b610ba9b33
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-06 22:02:06 +0100

    Use the newly created menu

--- GuardCord.py
+++ GuardCord.py
@@ -2,6 +2,7 @@ import time
 
 from colorama import *
 from helpers.time import Time
+from helpers.menus import Menus
 from helpers.sessions import Sessions
 
 init(convert=True)
@@ -9,12 +10,16 @@ init(convert=True)
 class GuardCord:
     def start():
         if Sessions.get() is not None:
-            print(f"{Fore.GREEN}[SUCCESS]{Fore.WHITE} Authorization is valid, Starting GuardCord.")
-            
             sessions: dict[str] = Sessions.get()
             
             for session in sessions["user_sessions"]:
-                print(Time.to_human_time(session["approx_last_used_time"]))
+                operating_system: str = session["client_info"]["os"]
+                platform: str = session["client_info"]["platform"]
+                location: str = session["client_info"]["location"]
+                readable_time: str = Time.to_human_time(session["approx_last_used_time"])
+                
+                menu_result: int = Menus.yes_or_no(f"{Fore.RED}[DEVICE] {operating_system.upper()}\n[PLATFORM] {platform}\n[LOCATION] {location}\n[TIME] {readable_time}\n")
+                
             
         else:
             print(f"{Fore.RED}[ERROR]{Fore.WHITE} Authorization is invalid, Please replace it.")
