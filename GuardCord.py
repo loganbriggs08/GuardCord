28217ad6b8a1ad71be258e165f7e973d8fbb05fd
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-06 20:54:27 +0100

    Print human readable time

--- GuardCord.py
+++ GuardCord.py
@@ -1,6 +1,7 @@
 import time
 
 from colorama import *
+from helpers.time import Time
 from helpers.sessions import Sessions
 
 init(convert=True)
@@ -13,7 +14,7 @@ class GuardCord:
             sessions: dict[str] = Sessions.get()
             
             for session in sessions["user_sessions"]:
-                print(session)
+                print(Time.to_human_time(session["approx_last_used_time"]))
             
         else:
             print(f"{Fore.RED}[ERROR]{Fore.WHITE} Authorization is invalid, Please replace it.")
