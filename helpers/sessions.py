2a54e2f30e2a9b9042fbffa026848a56735e844f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-06 20:36:27 +0100

    Manually input super_properties

--- helpers/sessions.py
+++ helpers/sessions.py
@@ -2,19 +2,16 @@ import httpx
 
 from typing import Union
 from helpers.config import Fetch
-from helpers.encoding import Encoding
 from helpers.useragent import UserAgent
 
 httpx_client = httpx.Client(timeout=10)
-
-x_super_properties: dict[str] = {"os":"Windows", "browser":"Discord Client", "release_channel":"canary", "client_version":"1.0.60", "os_version":"10.0.22621", "os_arch":"x64", "system_locale":"en-GB", "client_build_number":196142, "native_build_number":32342, "client_event_source": None, "design_id":0}
 
 class Sessions:
     def get() -> Union[None, dict[str]]:
         headers = {
             "authorization": Fetch.authorization_token(),
             "user-agent": UserAgent.get_random(),
-            "x-super-properties": Encoding.string(str(x_super_properties)),
+            "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJjYW5hcnkiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC42MCIsIm9zX3ZlcnNpb24iOiIxMC4wLjIyNjIxIiwib3NfYXJjaCI6Ing2NCIsInN5c3RlbV9sb2NhbGUiOiJlbi1HQiIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5NjE0MiwibmF0aXZlX2J1aWxkX251bWJlciI6MzIzNDIsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGwsImRlc2lnbl9pZCI6MH0=",
         }
         
         sessions_response: object = httpx_client.get("https://canary.discord.com/api/v9/auth/sessions", headers=headers)
