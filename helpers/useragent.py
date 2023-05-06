17a6cd2e3e64eb7fbf34fb6be8b4e8d0b5dc2b86
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-06 20:14:15 +0100

    Add random useragent generator

---
+++ helpers/useragent.py
@@ -0,0 +1,11 @@
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
+
+software_names = [SoftwareName.CHROME.value]
+operating_systems = [OperatingSystem.WINDOWS.value, OperatingSystem.LINUX.value]
+
+user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
+
+class UserAgent:
+    def get_random() -> str:
+        return user_agent_rotator.get_random_user_agent()
