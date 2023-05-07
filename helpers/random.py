be1eb98862a14b63267cb0332184debc5da2e7a6
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-07 15:53:00 +0100

    Added random string generation

---
+++ helpers/random.py
@@ -0,0 +1,5 @@
+import random
+
+class Random:
+    def string(length: int) -> str:
+        return "".join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for i in range(length))
