d79750119709f7a7403cbf4d407f9b9a5d7460c0
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-07 15:12:35 +0100

    Get codes by using the old nonce

--- helpers/discord.py
+++ helpers/discord.py
@@ -5,7 +5,7 @@ from helpers.config import Fetch
 httpx_client = httpx.Client(timeout=10)
 
 class Discord:
-    def send_code_to_email(password: str) -> bool:
+    def send_code_to_email(password: str) -> str:
         headers = {
             "authorization": Fetch.authorization_token(),
             "content-type": "application/json",
@@ -18,6 +18,25 @@ class Discord:
         response = httpx_client.post("https://canary.discord.com/api/v9/auth/verify/view-backup-codes-challenge", headers=headers, json=payload)
         
         if response.status_code == 200:
-            return True 
+            return response.json()["nonce"]
         else:
-            return False
+            None
+        
+    def get_codes(code: str, nonce: str) -> dict:
+        headers = {
+            "authorization": Fetch.authorization_token(),
+            "content-type": "application/json",
+        }
+        
+        payload = {
+            "key": code,
+            "nonce": nonce,
+            "regenerate": False,
+        }
+        
+        response = httpx_client.post("https://canary.discord.com/api/v9/users/@me/mfa/codes-verification", headers=headers, json=payload)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            None
