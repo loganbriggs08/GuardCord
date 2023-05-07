3a1ad31d6eeb7e169ce04917976f9aa7a5435c86
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-07 12:31:02 +0100

    Removed approx_last_used_time

--- helpers/database.py
+++ helpers/database.py
@@ -5,13 +5,20 @@ cursor: object = connection.cursor()
 
 class Database:
     def create_table():
-        cursor.execute("CREATE TABLE IF NOT EXISTS sessions (id_hash VARCHAR(255), approx_last_used_time VARCHAR(255), operating_system VARCHAR(255), platform VARCHAR(255))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS sessions (id_hash VARCHAR(255), operating_system VARCHAR(255), platform VARCHAR(255))")
         connection.commit()
         
     def get_sessions():
-        cursor.execute("SELECT id_hash FROM sessions")
-        return cursor.fetchall()
+        return_array: list[str] = []
+        
+        result = cursor.execute("SELECT id_hash FROM sessions").fetchall()
+        result_length: int = len(result)
+        
+        for i in range(result_length):
+            return_array.append(result[i][0])
+        
+        return return_array
     
-    def add_session(id_hash: str, approx_last_used_time: str, operating_system: str, platform: str):
-        cursor.execute("INSERT INTO sessions VALUES (?, ?, ?, ?)", (id_hash, approx_last_used_time, operating_system, platform))
+    def add_session(id_hash: str, operating_system: str, platform: str):
+        cursor.execute("INSERT INTO sessions VALUES (?, ?, ?)", (id_hash, operating_system, platform))
         connection.commit()
