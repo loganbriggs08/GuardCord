16ad9c3e2656c09dcddefd47ae09f92d0541735e
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-07 12:21:14 +0100

    Create get_sesssions function

--- helpers/database.py
+++ helpers/database.py
@@ -5,5 +5,9 @@ cursor: object = connection.cursor()
 
 class Database:
     def create_table():
-        cursor.execute("CREATE TABLE IF NOT EXISTS sessions (id_hash TEXT, approx_last_used_time TEXT, operating_system TEXT, platform TEXT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS sessions (id_hash VARCHAR(255), approx_last_used_time VARCHAR(255), operating_system VARCHAR(255), platform VARCHAR(255))")
         connection.commit()
+        
+    def get_sessions():
+        cursor.execute("SELECT id_hash FROM sessions")
+        return cursor.fetchall()
