17705b30adeee6684a4afd5f18f6e895ee6e5f59
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-15 10:25:43 +0100

    Add icon and threading to notifications

--- helpers/notifications.py
+++ helpers/notifications.py
@@ -4,4 +4,4 @@ toast_notifier_client: object = ToastNotifier()
 
 class Notifications:
     def send(post_title: str, post_body: str, duration: int) -> bool:
-        toast_notifier_client.show_toast(post_title, post_body, duration=duration)
+        toast_notifier_client.show_toast(post_title, post_body, duration=duration, icon_path="./assets/discord_logo.png", threaded=True)
