040fa68cca1080e50eb70c93516360b32cd1b91c
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-16 19:29:40 +0100

    Fix typo in code

--- modules/database.py
+++ modules/database.py
@@ -6,7 +6,7 @@ cursor: object = connection.cursor()
 class Database:
     def create_table():
         cursor.execute("CREATE TABLE IF NOT EXISTS sessions (id_hash VARCHAR(255), operating_system VARCHAR(255), platform VARCHAR(255))")
-        cursor.execute("CREATE TABLE IF NOT EXISTS account_information (email VARCHAR(255), password VARCHAR(255), token VARCHAR(255)))")
+        cursor.execute("CREATE TABLE IF NOT EXISTS account_information (email VARCHAR(255), password VARCHAR(255), token VARCHAR(255))")
         # cursor.execute("CREATE TABLE IF NOT EXISTS backup_codes (code VARCHAR(255))")
         connection.commit()
         
