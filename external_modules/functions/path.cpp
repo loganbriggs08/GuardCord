218a6ab828e0fa40ea1c566678f4f8c26eda022b
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-05-22 17:48:58 +0100

    If file exists return 1 else return 0

--- external_modules/functions/path.cpp
+++ external_modules/functions/path.cpp
@@ -1,5 +1,12 @@
 #include <string>
+#include <filesystem>
 
-int check_for_path(std::string path) {
+int check_for_file_path(std::string path_location) {
+    std::filesystem::path file_path(path_location);
 
+    if (std::filesystem::exists(file_path) == true) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
